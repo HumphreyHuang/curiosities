d81b3b15b4bb346507cfddb0dc90a9d8e0513a17
humphreyhuang <humphrey.dev@gmail.com>
2019-10-22 22:25:24 -0400

    WIP - Styles

---
+++ pages/_app.js
@@ -0,0 +1,34 @@
+import App from 'next/app';
+import React from 'react';
+import { ThemeProvider, createGlobalStyle } from 'styled-components';
+
+const theme = {
+    colors: {
+        white: '#FFFFFF',
+        background: '#FAFAFA'
+    }
+};
+
+const GlobalStyle = createGlobalStyle`
+    box-sizing: border-box;
+    font-style: normal;
+    body {
+        margin: 0;
+    }
+    a {
+        color: inherit;
+        text-decoration: none;
+    }
+`;
+
+export default class MyApp extends App {
+    render() {
+        const { Component, pageProps } = this.props;
+        return (
+            <ThemeProvider theme={theme}>
+                <GlobalStyle />
+                <Component {...pageProps} />
+            </ThemeProvider>
+        );
+    }
+}

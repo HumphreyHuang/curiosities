738e230e79a883bc35188a6bb188c24b72c504df
humphreyhuang <humphrey.dev@gmail.com>
2019-11-18 22:50:26 -0500

    Removed dotenv config

--- next.config.js
+++ next.config.js
@@ -1,23 +1,3 @@
 const withCSS = require('@zeit/next-css');
-require('dotenv').config();
 
-const path = require('path');
-const Dotenv = require('dotenv-webpack');
-
-module.exports = withCSS({
-    webpack: config => {
-        config.plugins = config.plugins || [];
-
-        config.plugins = [
-            ...config.plugins,
-
-            // Read the .env file
-            new Dotenv({
-                path: path.join(__dirname, '.env'),
-                systemvars: true
-            })
-        ];
-
-        return config;
-    }
-});
+module.exports = withCSS({});
